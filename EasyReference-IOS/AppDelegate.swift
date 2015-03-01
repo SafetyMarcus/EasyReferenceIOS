f421b7e0151805e66caca7f739207154bc033aed
Marcus Hooper <hooperarma@gmail.com>
2015-03-01 22:31:11 +1000

    Setting up for when I change to UIViewController

--- EasyReference-IOS/AppDelegate.swift
+++ EasyReference-IOS/AppDelegate.swift
@@ -15,8 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var referenceLists: [ReferenceList] = []
     
+    func uiColorFromHex(rgbValue:UInt32) -> UIColor
+    {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:1.0)
+    }
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        var navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.tintColor = uiColorFromHex(0xF20505)
+        navigationBarAppearance.barTintColor = uiColorFromHex(0xF20505)
+        navigationBarAppearance.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         return true
     }
 
