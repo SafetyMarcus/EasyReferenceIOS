bc866e9672207ae6bd9aaf3e1af23a5272762c08
Marcus Hooper <hooperarma@gmail.com>
2015-04-05 15:28:27 +1000

    Added book chapter values

--- EasyReference-IOS/ReferenceItem.swift
+++ EasyReference-IOS/ReferenceItem.swift
@@ -35,6 +35,11 @@ class ReferenceItem
     var pageNumber = ""
     var doi = ""
     
+    //Book Chapter Values
+    var editors = ""
+    var bookTitle = ""
+    var bookSubtitle = ""
+    
     init(type: ReferenceType)
     {
         self.type = type
@@ -49,6 +54,10 @@ class ReferenceItem
         else if(type == ReferenceType.Journal)
         {
             return getJournalReferenceString()
+        }
+        else if(type == ReferenceType.BookChapter)
+        {
+            return getBookChapterReferenceString()
         }
         
         return ""
@@ -77,6 +86,22 @@ class ReferenceItem
         fullString += getIssueNumberForReference()
         fullString += getPageNumberForReference()
         fullString += getDoiForReference()
+        
+        return fullString
+    }
+    
+    func getBookChapterReferenceString() -> String
+    {
+        var fullString = getAuthorForReference()
+        fullString += getDateForReference()
+        fullString += getTitleForReference()
+        fullString += getSubtitleForReference()
+        fullString += getEditorsForReference()
+        fullString += getBookTitleForReference()
+        fullString += getBookSubtitleForReference()
+        fullString += getPageNumberForReference()
+        fullString += getLocationForReference()
+        fullString += getPublisherForReference()
         
         return fullString
     }
@@ -191,6 +216,36 @@ class ReferenceItem
         return " doi:\(doi)"
     }
     
+    func getEditorsForReference() -> String
+    {
+        if(editors.isEmpty)
+        {
+            return ""
+        }
+        
+        return " In \(editors) (Eds)"
+    }
+    
+    func getBookTitleForReference() -> String
+    {
+        if(bookTitle.isEmpty)
+        {
+            return ""
+        }
+        
+        return " \(bookTitle), "
+    }
+    
+    func getBookSubtitleForReference() -> String
+    {
+        if(bookSubtitle.isEmpty)
+        {
+            return ""
+        }
+        
+        return " \(bookSubtitle), "
+    }
+    
     func getNumberOfCells() -> NSInteger
     {
         var cellCount = 0
@@ -203,13 +258,17 @@ class ReferenceItem
         {
             cellCount = 9
         }
+        else if(self.type == ReferenceType.BookChapter)
+        {
+            cellCount = 10
+        }
         
         return cellCount
     }
     
     func getLabelsForCells() -> [NSString]
     {
-        var labels: [NSString] = [NSString]()
+        var labels = [NSString]()
         
         if(self.type == ReferenceType.Book)
         {
@@ -219,27 +278,31 @@ class ReferenceItem
         {
             labels = self.getJournalLabels()
         }
+        else if(self.type == ReferenceType.BookChapter)
+        {
+            labels = self.getBookChapterLabels()
+        }
         
         return labels
     }
     
     private func getBookLabels() -> [NSString]
     {
-        var labels: [NSString] = [NSString]()
+        var labels = [NSString]()
         
         labels.append("Author")
-        labels.append("Date")
+        labels.append("Year")
         labels.append("Title")
         labels.append("Subtitle")
-        labels.append("Publisher")
         labels.append("Location")
+        labels.append("Publisher")
         
         return labels
     }
     
     private func getJournalLabels() -> [NSString]
     {
-        var labels: [NSString] = [NSString]()
+        var labels = [NSString]()
         
         labels.append("Author")
         labels.append("Year")
@@ -254,9 +317,27 @@ class ReferenceItem
         return labels
     }
     
+    private func getBookChapterLabels() -> [NSString]
+    {
+        var labels = [NSString]()
+        
+        labels.append("Author")
+        labels.append("Year")
+        labels.append("Chapter Title")
+        labels.append("Chapter Subtitle")
+        labels.append("Editors")
+        labels.append("Book Title")
+        labels.append("Book Subtitle")
+        labels.append("Pages of Chapter")
+        labels.append("Location")
+        labels.append("Publisher")
+        
+        return labels
+    }
+    
     func getHintsForCells() -> [NSString]
     {
-        var hints: [NSString] = [NSString]()
+        var hints = [NSString]()
         
         if(self.type == ReferenceType.Book)
         {
@@ -266,30 +347,34 @@ class ReferenceItem
         {
             hints = self.getJournalHints()
         }
+        else if(self.type == ReferenceType.BookChapter)
+        {
+            hints = self.getBookChapterHints()
+        }
         
         return hints
     }
     
     private func getBookHints() -> [NSString]
     {
-        var hints: [NSString] = [NSString]()
+        var hints = [NSString]()
         
         hints.append("enter author")
-        hints.append("enter date")
+        hints.append("enter year")
         hints.append("enter title")
         hints.append("enter subtitle")
-        hints.append("enter publisher")
         hints.append("enter location")
+        hints.append("enter publisher")
         
         return hints
     }
 
     private func getJournalHints() -> [NSString]
     {
-        var hints: [NSString] = [NSString]()
+        var hints = [NSString]()
         
         hints.append("enter author")
-        hints.append("enter date")
+        hints.append("enter year")
         hints.append("enter title")
         hints.append("enter subtitle")
         hints.append("enter journal title")
@@ -297,6 +382,24 @@ class ReferenceItem
         hints.append("enter issue number")
         hints.append("enter page number")
         hints.append("enter doi/url")
+        
+        return hints
+    }
+    
+    private func getBookChapterHints() -> [NSString]
+    {
+        var hints = [NSString]()
+
+        hints.append("enter author")
+        hints.append("enter year")
+        hints.append("enter chapter title")
+        hints.append("enter chapter subtitle")
+        hints.append("enter editors")
+        hints.append("enter book title")
+        hints.append("enter book subtitle")
+        hints.append("enter pages of chapter")
+        hints.append("enter location")
+        hints.append("enter publisher")
         
         return hints
     }
@@ -312,6 +415,10 @@ class ReferenceItem
         else if(type == ReferenceType.Journal)
         {
             value = getJournalValueForPosition(position)
+        }
+        else if(type == ReferenceType.BookChapter)
+        {
+            value = getBookChapterValueForPosition(position)
         }
         
         return value
@@ -337,11 +444,11 @@ class ReferenceItem
         }
         else if(position == 4)
         {
-            return self.publisher
+            return self.location
         }
         else if(position == 5)
         {
-            return self.location
+            return self.publisher
         }
         
         return ""
@@ -389,6 +496,52 @@ class ReferenceItem
         return ""
     }
     
+    private func getBookChapterValueForPosition(position: NSInteger) -> NSString
+    {
+        if(position == 0)
+        {
+            return self.author
+        }
+        else if(position == 1)
+        {
+            return self.date
+        }
+        else if(position == 2)
+        {
+            return self.title
+        }
+        else if(position == 3)
+        {
+            return self.subTitle
+        }
+        else if(position == 4)
+        {
+            return self.editors
+        }
+        else if(position == 5)
+        {
+            return self.bookTitle
+        }
+        else if(position == 6)
+        {
+            return self.bookSubtitle
+        }
+        else if(position == 7)
+        {
+            return self.pageNumber
+        }
+        else if(position == 8)
+        {
+            return self.location
+        }
+        else if(position == 9)
+        {
+            return self.publisher
+        }
+        
+        return ""
+    }
+    
     func saveValueForPosition(position: NSInteger, value: NSString)
     {
         if(type == ReferenceType.Book)
@@ -398,6 +551,10 @@ class ReferenceItem
         else if(type == ReferenceType.Journal)
         {
             saveJournalValueForPosition(position, value: value)
+        }
+        else if(type == ReferenceType.BookChapter)
+        {
+            saveBookChapterValueForPosition(position, value: value)
         }
     }
     
@@ -421,11 +578,11 @@ class ReferenceItem
         }
         else if(position == 4)
         {
-            self.publisher = value
+            self.location = value
         }
         else if(position == 5)
         {
-            self.location = value
+            self.publisher = value
         }
     }
     
@@ -466,6 +623,50 @@ class ReferenceItem
         else if(position == 8)
         {
             self.doi = value
+        }
+    }
+    
+    private func saveBookChapterValueForPosition(position: NSInteger, value: NSString)
+    {
+        if(position == 0)
+        {
+            self.author = value
+        }
+        else if(position == 1)
+        {
+            self.date = value
+        }
+        else if(position == 2)
+        {
+            self.title = value
+        }
+        else if(position == 3)
+        {
+            self.subTitle = value
+        }
+        else if(position == 4)
+        {
+            self.editors = value
+        }
+        else if(position == 5)
+        {
+            self.bookTitle = value
+        }
+        else if(position == 6)
+        {
+            self.bookSubtitle = value
+        }
+        else if(position == 7)
+        {
+            self.pageNumber = value
+        }
+        else if(position == 8)
+        {
+            self.location = value
+        }
+        else if(position == 9)
+        {
+            self.publisher = value
         }
     }
 }
